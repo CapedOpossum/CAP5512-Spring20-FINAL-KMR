b098e307a551298420c88f360f013d84fbdd76ed
Matthew Kurtz <MatthewRyanKurtz@gmail.com>
2020-04-18 13:50:12 -0400

    Added C# WPF application

---
+++ tictactoe/TicTacToe/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.ViewModels
+{
+    public class MainViewModel
+    {
+        public TicTacToeViewModel TicTacToeViewModel { get; set; }
+
+        public MainViewModel()
+        {
+            TicTacToeViewModel = new TicTacToeViewModel();
+        }
+    }
+}
---
+++ tictactoe/TicTacToe/ViewModels/TicTacToeViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Models;
+
+namespace TicTacToe.ViewModels
+{
+    public class TicTacToeViewModel : INotifyPropertyChanged
+    {
+        private string myVar = "non";
+
+        public string Button1Text
+        {
+            get { return myVar; }
+            set
+            {
+                myVar = value;
+                NotifyOfPropertyChanged(nameof(Button1Text));
+            }
+        }
+
+        public Command Button1Command
+        {
+            get
+            {
+                return new Command(o =>
+                {
+                    Console.WriteLine("Button pushed");
+                    Button1Text = "New text";
+                });
+            }
+        }
+
+        private void NotifyOfPropertyChanged(string v)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
